05925202ed5d191a150eb4f3fdead6e8c63735e9
hh <hh@w10.com>
2020-01-10 21:04:58 +0100

    more fixes

--- AvrBuild.bat
+++ AvrBuild.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\Users\hh\Documents\GitHub\MPA\labels.tmp" -fI -W+ie -o "C:\Users\hh\Documents\GitHub\MPA\sudok85.hex" -d "C:\Users\hh\Documents\GitHub\MPA\sudok85.obj" -e "C:\Users\hh\Documents\GitHub\MPA\sudok85.eep" -m "C:\Users\hh\Documents\GitHub\MPA\sudok85.map" "C:\Users\hh\Documents\GitHub\MPA\sudok85.asm"
+"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\Users\hh\Documents\GitHub\MPA\labels.tmp" -fI -W+ie -o "C:\Users\hh\Documents\GitHub\MPA\sudoku.hex" -d "C:\Users\hh\Documents\GitHub\MPA\sudoku.obj" -e "C:\Users\hh\Documents\GitHub\MPA\sudoku.eep" -m "C:\Users\hh\Documents\GitHub\MPA\sudoku.map" "C:\Users\hh\Documents\GitHub\MPA\sudoku.asm"
